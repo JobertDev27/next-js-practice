d51b3b3a6dc561e92eefc5a4cb8167c76bcc5ee2
JobertDev27 <jobertdev27@gmail.com>
2025-07-05 22:53:43 +0800

    fix: updates css defaults

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
+import TodoBox from "./TodoBox";
+
 export default function Home() {
-  return <h1>Hello world</h1>;
+  return (
+    <main>
+      <h1 className="text-2xl mt-10 ml-5">TODO LIST</h1>
+
+      <div className="mt-5 ml-10">
+        <TodoBox task={"Clean house"} />
+      </div>
+    </main>
+  );
 }
