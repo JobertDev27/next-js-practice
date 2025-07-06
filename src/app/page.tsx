c380410f7262028a00955644a3243218f07da98d
JobertDev27 <jobertdev27@gmail.com>
2025-07-06 15:30:48 +0800

    fix: adds a way to add tasks and removed on click for now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,36 @@
+"use client";
 import { useState } from "react";
 import TodoBox from "./TodoBox";
 
 export default function Home() {
-  const [task, setTask] = useState<string[]>([]);
-
-  handleClick = (id: number) => {
-    setTask(task.filter(task.id));
-  };
+  const [textValue, setTextValue] = useState<string>("");
+  const [tasks, setTasks] = useState<string[]>(["test"]);
 
   return (
     <main>
       <h1 className="text-2xl mt-10 ml-5">TODO LIST</h1>
-
+      <div>
+        <p>Add task</p>
+        <input
+          type="text"
+          name=""
+          id="task-input"
+          value={textValue}
+          onChange={(e) => setTextValue(e.target.value)}
+        />
+        <button
+          onClick={() => {
+            setTasks((t) => [...t, textValue]);
+            setTextValue("");
+          }}
+        >
+          add
+        </button>
+      </div>
       <div className="mt-5 ml-10">
-        <TodoBox task={"Clean house"} />
+        {tasks.map((task: string, key: number) => {
+          return <TodoBox task={task} key={key} />;
+        })}
       </div>
     </main>
   );
